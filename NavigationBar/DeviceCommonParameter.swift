171c4a4ccedad65aa4e89246d551d321c9a2677c
Superman <703353105@qq.com>
2018-07-25 16:54:04 +0800

    first commit

---
+++ NavigationBar/DeviceCommonParameter.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceCommonParameter.swift
+//  NavigationBar
+//
+//  Created by Superman on 2018/7/25.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+import UIKit
+
+class DeviceCommonParameter: NSObject {
+
+}
